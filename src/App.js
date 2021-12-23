f7c7744da0f0a4e5b99c29e584d6350eda3c8ae5
kate0young <bwings2001@naver.com>
2021-12-23 17:26:00 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import MyComponent from './MyComponent'
+
+function App() {
+	return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
+}
+
+export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MyComponent extends Component {
+	constructor(props) {
+		super(props);
+		// state의 초기값 설정하기
+		this.state = {
+			number: 0
+		};
+	}
+	
+	render() {
+		const { number } = this.state; // state를 조회할 때는 this.state로 조회
+		return (
+			<div>
+				<h1>{ number }</h1>
+				<button
+					// onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정.form
+					onClick={()=>{
+							// this.setState를 사용하여 state에 새로운 값을 넣을 수 있음.
+							this.setState({ number: number + 1});
+						}}
+					>
+					+1
+				</button>
+			</div>
+		);
+	}
+}
+
+
+MyComponent.defaultProps = {
+	name: '기본이름'
+}
+
+MyComponent.propTypes = {
+	name : PropTypes.string,
+	favoriteNumber: PropTypes.number.isRequired
+	
+}
+
+export default MyComponent;
